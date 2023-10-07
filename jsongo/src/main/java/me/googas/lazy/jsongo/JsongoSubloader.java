958cf105d911353d37c64a3a21586a8bd4c7d916
Chevyself <chevyself@gmail.com>
2023-10-07 06:57:23 -0500

    Delete unused first queried element

--- jsongo/src/main/java/me/googas/lazy/jsongo/JsongoSubloader.java
+++ jsongo/src/main/java/me/googas/lazy/jsongo/JsongoSubloader.java
@@ -101,7 +101,6 @@ public abstract class JsongoSubloader<T> implements Subloader {
    */
   protected boolean save(@NonNull Bson query, @NonNull T object) {
     Document document = Document.parse(this.parent.getGson().toJson(object));
-    Bson first = this.collection.find(query).first();
     return this.collection
         .replaceOne(query, document, new ReplaceOptions().upsert(true))
         .wasAcknowledged();
